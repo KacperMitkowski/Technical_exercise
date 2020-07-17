2d81240c40c7bbda7997cd64dc4b0655fdaa39c7
Kacper Mitkowski <kacper.mitkowski@gmail.com>
2020-07-17 12:15:10 +0200

    tests finished

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ def add_expressions(*first_args):
 
         if not all(isinstance(arg, int) for arg in elements):
             raise TypeError("not int arg in function")
+        if len(first_args) == 0 or len(second_args) == 0:
+            raise TypeError("no arguments given to function")
 
-        d = {}
+        result_dict = {}
         for el in elements:
             if elements.count(el) > 1:
-                d[el] = el * 2
+                result_dict[el] = el * 2
             else:
-                d[el] = el
-        return d
+                result_dict[el] = el
+        return result_dict
 
     return helper
 
-# try:
-#     outcome = add_expressions(1, 2, 8)(2, 4)
-#     result = ""
-#     for key, value in outcome.items():
-#         result += f'{value}X**{key} + '
-#     result += '0'
-#
-#     print(result)
-# except TypeError as e:
-#     print(e)
+
+try:
+    outcome = add_expressions(1, 1, 2)(2, 4)
+    result = ""
+    for key, value in outcome.items():
+        result += f'{value}X**{key} + '
+    result += '0'
+
+    print(result)
+except TypeError as e:
+    print(e)
--- tests.py
+++ tests.py
@@ -11,7 +11,19 @@ class TestAddingExpressions(unittest.TestCase):
         self.assertRaises(TypeError, add_expressions(1, 2, "test")(2, 4, 9))
         self.assertRaises(TypeError, add_expressions(1, 2)(2, 4, 9, "test"))
         self.assertRaises(TypeError, add_expressions(1, 2)(2, 4, 9, None))
-        self.assertRaises(TypeError, add_expressions(1, 2, None)(2, 4, 9,))
+        self.assertRaises(TypeError, add_expressions(1, 2, None)(2, 4, 9, ))
+
+    def check_if_giving_no_args_to_func_raises_type_error(self):
+        self.assertRaises(TypeError, add_expressions()())
+        self.assertRaises(TypeError, add_expressions(1,2,3)())
+        self.assertRaises(TypeError, add_expressions()(1,2,3))
 
     def test_if_add_expressions_returns_correct_values(self):
-        print(add_expressions(1, 2, 8)(2, 4))
+        self.assertDictEqual(add_expressions(1, 2, 8)(2, 4), {1: 1, 2: 4, 8: 8, 4: 4})
+        self.assertDictEqual(add_expressions(1, 1, 2)(2, 4), {1: 2, 2: 4, 4: 4})
+        self.assertDictEqual(add_expressions(1, 1, 2)(0, 4), {1: 2, 2: 2, 0: 0, 4: 4})
+
+    def test_if_keys_or_values_in_result_dict_are_int(self):
+        for key, value in add_expressions(1, 2, 8)(2, 3).items():
+            self.assertIsInstance(key, int)
+            self.assertIsInstance(value, int)
